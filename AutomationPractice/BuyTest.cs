9f32142781e368a698e3deb6d3d347c71a5f41cf
Alejandro Perdomo <aperdomobo@gmail.com>
2019-02-09 14:32:53 -0500

    missing files

---
+++ AutomationPractice/BuyTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace AutomationPractice
+{
+  [TestFixture]
+  public class BuyTest
+  {
+    [Test]
+    public void Example()
+    {
+      IWebDriver firefoxDriver = new FirefoxDriver();
+
+    }
+  }
+}
